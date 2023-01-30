2eda7758d6ba48b5dea22eccd1d22d72289f4f42
sinasamaki <sinasamaki@protonmail.com>
2023-01-30 06:26:28 +0100

    simplified shader code based on @romainguy suggestion

--- app/src/main/java/com/sinasamaki/metaballs/MetaContainter.kt
+++ app/src/main/java/com/sinasamaki/metaballs/MetaContainter.kt
@@ -7,44 +7,24 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.onSizeChanged
 import org.intellij.lang.annotations.Language
 
 @Language("AGSL")
 const val ShaderSource = """
     uniform shader composable;
     
-    uniform float2 size;
     uniform float cutoff;
-    uniform float r;
-    uniform float g;
-    uniform float b;
+    uniform float3 rgb;
     
     half4 main(float2 fragCoord) {
-    
         half4 color = composable.eval(fragCoord);
-        
-        float alpha = color.a;
-        if (alpha > cutoff) {
-            alpha = 1.0;
-        } else {
-            alpha = 0.0;
+        color.a = step(cutoff, color.a);
+        if (color == half4(0.0, 0.0, 0.0, 1.0)) {
+            color.rgb = half3(rgb[0], rgb[1], rgb[2]);
         }
-        
-        float red = color.r;
-        float green = color.g;
-        float blue = color.b;
-        if (red == 0.0 && green == 0.0 && blue == 0.0 && alpha == 1.0) {
-            red = r;
-            green = g;
-            blue = b;
-        }
-        
-        color = half4(red, green, blue, alpha);
         return color;
     }
 """
@@ -61,27 +41,15 @@ fun MetaContainer(
     }
     Box(
         modifier
-//            .clipToBounds()
-            .onSizeChanged {
-                metaShader.setFloatUniform(
-                    "size",
-                    it.width.toFloat(),
-                    it.height.toFloat(),
-                )
-            }
             .graphicsLayer {
-                alpha = .99f
                 metaShader.setFloatUniform("cutoff", cutoff)
-                metaShader.setFloatUniform("r", color.red)
-                metaShader.setFloatUniform("g", color.green)
-                metaShader.setFloatUniform("b", color.blue)
+                metaShader.setFloatUniform("rgb", color.red, color.green, color.blue)
                 renderEffect = RenderEffect
                     .createRuntimeShaderEffect(
                         metaShader, "composable"
                     )
                     .asComposeRenderEffect()
-            }
-                ,
+            },
         content = content,
     )
 }
